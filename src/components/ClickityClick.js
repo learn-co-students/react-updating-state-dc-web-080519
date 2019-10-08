3b935ad343f00a1816482e8afb7eb937b11976ef
Peyton Doyle <p6doyle@gmail.com>
2019-10-08 16:05:43 -0400

    Done.

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,13 @@
-// Code ClickityClick Component Here
+this.setState({
+  addressInfo: {
+    ...this.state.addressInfo,
+    city: 'New York City'
+  }
+});
+
+handleClick = () => {
+  this.setState({
+    hasBeenClicked: true
+  })
+  console.log(this.state.hasBeenClicked); // prints false
+}
